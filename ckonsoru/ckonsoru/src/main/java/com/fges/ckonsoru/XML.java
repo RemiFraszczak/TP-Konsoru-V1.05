7ffab715a0a58d6e4477f51da2c90366bb138f60
RemiFraszczak <91823880+RemiFraszczak@users.noreply.github.com>
2022-04-03 14:55:22 +0200

    Update XML.java

--- ckonsoru/ckonsoru/src/main/java/com/fges/ckonsoru/XML.java
+++ ckonsoru/ckonsoru/src/main/java/com/fges/ckonsoru/XML.java
@@ -1,15 +1,32 @@
 package com.fges.ckonsoru;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class XML {
 
-    public static void  condition1(String partiesdates[]){
-        XMLRequests test=new XMLRequests();
-        test.afficherCreneaux(Integer.parseInt(partiesdates[2]),Integer.parseInt(partiesdates[1]),Integer.parseInt(partiesdates[0]));
+    public static void condition1(String partiesdates[]) {
+        XMLRequests test = new XMLRequests();
+        test.afficherCreneaux(Integer.parseInt(partiesdates[2]), Integer.parseInt(partiesdates[1]), Integer.parseInt(partiesdates[0]));
     }
-    public static void condition2(String nomcli)
-    {
-    XMLRequests test = new XMLRequests();
-    test.afficheRDVCli(nomcli);
+
+    public static void condition2(String nomcli) {
+        XMLRequests test = new XMLRequests();
+        test.afficheRDVCli(nomcli);
+    }
+
+    public static void condition3(String daterdv, String nomcli, String nomveto) {
+        XMLRequests test = new XMLRequests();
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+            Date parsedDate = dateFormat.parse(daterdv);
+            Timestamp datetimestamp = new java.sql.Timestamp(parsedDate.getTime());
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(datetimestamp);
+            test.AddRdv(timeStamp, nomveto, nomcli);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
