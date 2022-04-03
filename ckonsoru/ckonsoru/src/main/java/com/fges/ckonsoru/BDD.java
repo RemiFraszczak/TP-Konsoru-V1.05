020e25f4390a2575efc1f2ca648175507b5e8d71
RemiFraszczak <91823880+RemiFraszczak@users.noreply.github.com>
2022-04-03 14:55:54 +0200

    Update BDD.java

--- ckonsoru/ckonsoru/src/main/java/com/fges/ckonsoru/BDD.java
+++ ckonsoru/ckonsoru/src/main/java/com/fges/ckonsoru/BDD.java
@@ -1,5 +1,9 @@
 package com.fges.ckonsoru;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BDD {
     public static void condition1(String partiesdates[]) {
         BDDRequests test = new BDDRequests();
@@ -8,5 +12,17 @@ public class BDD {
     public static void condition2(String nomcli) {
         BDDRequests test = new BDDRequests();
         test.listerdv(nomcli);
+    }
+    public static void condition3(String daterdv, String nomcli, String nomveto)
+    {
+        BDDRequests test = new BDDRequests();
+        try{
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+            Date parsedDate = dateFormat.parse(daterdv);
+            Timestamp datetimestamp = new java.sql.Timestamp(parsedDate.getTime());
+            test.prendrerdv(datetimestamp, nomveto, nomcli);
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
