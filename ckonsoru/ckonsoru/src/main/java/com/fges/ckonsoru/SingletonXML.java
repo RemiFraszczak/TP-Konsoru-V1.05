9f136868f053e445eeac6e4e196b3c7b44df07f6
RemiFraszczak <91823880+RemiFraszczak@users.noreply.github.com>
2022-04-17 20:43:42 +0200

    Add files via upload

---
+++ ckonsoru/ckonsoru/src/main/java/com/fges/ckonsoru/SingletonXML.java
@@ -0,0 +1,38 @@
+package com.fges.ckonsoru;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SingletonXML {
+    private static SingletonXML choix;
+
+    private SingletonXML() {}
+    public static SingletonXML getInstance() {
+        if (choix == null) {
+            choix = new SingletonXML();
+        }
+        return choix;
+    }
+    // le reste des méthodes
+    public void condition1(String partiesdates[]) {
+        BDDRequests test = new BDDRequests();
+        test.bdd_creneaux(Integer.parseInt(partiesdates[2]), Integer.parseInt(partiesdates[1]), Integer.parseInt(partiesdates[0]));
+    }
+    public void condition2(String nomcli) {
+        BDDRequests test = new BDDRequests();
+        test.listerdv(nomcli);
+    }
+    public void condition3(String daterdv, String nomcli, String nomveto)
+    {
+        BDDRequests test = new BDDRequests();
+        try{
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+            Date parsedDate = dateFormat.parse(daterdv);
+            Timestamp datetimestamp = new java.sql.Timestamp(parsedDate.getTime());
+            test.prendrerdv(datetimestamp, nomveto, nomcli);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
